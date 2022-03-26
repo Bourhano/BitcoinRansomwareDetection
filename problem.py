6123e154441b5ca10f7a50bca8356dffee16d810
Ronny Tonato <54164031+RonnyTZ@users.noreply.github.com>
2022-03-26 21:25:42 +0100

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,13 +1,11 @@
 import os
 
 import pandas as pd
-import numpy as np
 
 from sklearn.metrics import recall_score
 from sklearn.model_selection import ShuffleSplit
 
 import rampwf as rw
-from rampwf.score_types.base import BaseScoreType
 
 problem_title = "Bitcoin Ransomeware Detection"
 
@@ -18,48 +16,22 @@ Predictions = rw.prediction_types.make_multiclass(
 workflow = rw.workflows.Estimator()
 
 
-class RECALL(BaseScoreType):
-    is_lower_the_better = False
-    minimum = 0.0
-    maximum = 1.0
-
-    def __init__(self, name="RECALL", precision=4):
-        self.name = name
-        self.precision = precision
-
-    def __call__(self, y_true, y_pred):
-
-        # y_true = np.argmax(y_true, axis=1)
-        # y_pred = np.argmax(y_pred, axis=1)
-
-        score = recall_score(y_true=y_true, y_pred=y_pred, average='weighted')
-
-        return score
-
-
 score_types = [
-    # RECALL(name="RECALL"),
-    rw.score_types.ROCAUC(name='auc'),
-    # rw.score_types.Accuracy(name='acc'),
-    rw.score_types.NegativeLogLikelihood(name='nll'),
-    # rw.score_types.BrierScore(name='brier_score')
+    rw.score_types.ROCAUC(name='auc')
 ]
 
 
-_ignore_column_names = ['address', 'label']
+_ignore_column_names = 'address'
+_target_column_name = 'label'
 
 
 def _get_data(path, f_name):
 
     dataset = pd.read_csv(os.path.join(path, 'data', f_name), sep=",")
-    # print(dataset)
-    X = dataset.drop(_ignore_column_names, axis=1)
-    y = np.array(np.where(dataset["label"] == "white", 0, 1))
 
-    # print(X.columns)
-    # print(y)
-    # X = X_df.to_numpy()
-    # y = np.reshape(y_df.to_numpy(), (y_df.shape, ))
+    X = dataset.drop([_target_column_name] + [_ignore_column_names], axis=1)
+    y = dataset[_target_column_name].values
+
     return X, y
 
 
