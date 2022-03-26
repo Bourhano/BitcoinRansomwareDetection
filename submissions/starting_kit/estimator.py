f4b783f945d594a428e0a08df043d063075063e1
Ronny Tonato <54164031+RonnyTZ@users.noreply.github.com>
2022-03-26 23:11:53 +0100

    Update estimator.py

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from datetime import date
@@ -5,33 +6,32 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.pipeline import Pipeline, make_pipeline
 from pandas import Timestamp
-from sklearn.linear_model import LogisticRegression
-
+from sklearn.ensemble import RandomForestClassifier
 
 def _process_time_feature(X):
     '''From the columns "year" and "day", we create a datetime
     column day/month/year'''
     '''For example, for year=2017 and day=130,
     we will get 10/05/2017'''
+    years = np.array(X['year'])
+    days = np.array(X['day'])
     dat_temp = []
     for i in range(X.shape[0]):
         aux = date.fromordinal(
-                date(X['year'][i], 1, 1).toordinal()
-                + X['day'][i]-1).strftime('%d/%m/%Y')
+                 date(years[i], 1, 1).toordinal()
+                + days[i]-1).strftime('%d/%m/%Y')
+        
         dat_temp.append(aux)
 
-    X['time'] = dat_temp
-    X['time'] = pd.to_datetime(X['time'])
-    X.pop('day')
-    X.pop('year')
-    return X['time'].values
+    X['time'] = pd.to_datetime(dat_temp)
+    return X['time']
 
 
 def get_estimator():
 
     cnt_featnames = ['length', 'weight',
                      'count', 'looped', 'neighbors', 'income']
-    date_featnames = ['time']
+    date_featnames = ['year', 'day']
 
     '''First we create a scikit-learn encoder that computes the age in
                     days of columns containing dates'''
@@ -41,7 +41,9 @@ def get_estimator():
             return self
 
         def transform(self, X):
-            return X.apply(lambda x: (x - self.today).dt.days, axis=0)
+            aux = (X-self.today).dt.days
+            return aux.values.reshape((len(aux),-1))
+            # return X.apply(lambda x: (x - self.today).days).reshape((X.shape[0], -1))
 
     '''A pipeline that first computes age, and standardizes it'''
     scaled_age_encoder = FunctionTransformer(
@@ -61,7 +63,7 @@ def get_estimator():
     ])
 
     '''Now, we define a classifier'''
-    classifier = LogisticRegression()
+    classifier = RandomForestClassifier(n_estimators=100, random_state=42,class_weight='balanced')
 
     '''We wrap all in a pipeline'''
     pipeline = Pipeline(steps=[
