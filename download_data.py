21038721f247c665341d8e571706d836ae1c05b4
bourhan dernayka <hanodern@gmail.com>
2022-03-27 01:28:29 +0100

    adapt download_data.py, update gitignore and environment.yml

--- download_data.py
+++ download_data.py
@@ -1,19 +1,57 @@
-import zipfile
-from download import download
+import os
+import requests
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# file source: https://archive.ics.uci.edu/ml/machine-learning-databases/00526/data.zip
+url = "https://www.dropbox.com/s/gpz7730z5ta1g1p/BitcoinHeistData.csv?dl=1"
+
+public_path = "./data/public/"
+private_path = "./data/"
+file_name = 'BitcoinHeistData.csv'
+file_path = os.path.join(private_path, file_name)
+
+try:
+    os.stat(private_path)
+except OSError as e:
+    os.mkdir(private_path)
+
+try:
+    os.stat(public_path)
+except OSError as e:
+    os.mkdir(public_path)
 
 print("Downloading the data file. This could take a couple of minutes...")
-
-url = "https://www.dropbox.com/s/anqe5pwawb0gpaw/data.zip?dl=0"
-
-path = "./data/data.zip"
-
-path = download(url=url, path=path, progressbar=True, replace=True)
-
+r = requests.get(url, allow_redirects=True)
+open(file_path, 'wb').write(r.content)
 print("File downloaded...")
 
-print("Extracting data.zip in ./data/")
+df = pd.read_csv(file_path)
+df.loc[df['label'] != 'white', 'label'] = 1
+df.loc[df['label'] == 'white', 'label'] = -1
 
-with zipfile.ZipFile(path, 'r') as zip_ref:
-    zip_ref.extractall('./data/')
-    
+Y = df[["label"]]
+X = df.drop(columns=["label"])
+
+X_public, X_hidden, Y_public, Y_hidden = train_test_split(
+    X, Y, random_state=42, test_size=0.3, stratify=Y, shuffle=True
+)
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X_public, Y_public, random_state=42, test_size=0.3, stratify=Y_public, shuffle=True
+)
+
+X_hidden['label'] = Y_hidden
+X_train['label'] = Y_train
+X_test['label'] = Y_test
+
+private_set = X_hidden
+public_train = X_train
+public_test = X_test
+
+private_set.to_csv(os.path.join(private_path, "private.csv.gzip"), index=False, compression="gzip")
+public_train.to_csv(os.path.join(public_path, "train.csv.gzip"), index=False, compression="gzip")
+public_test.to_csv(os.path.join(public_path, "test.csv.gzip"), index=False, compression="gzip")
+
+os.remove(file_path)
 print("Data extracted...")
