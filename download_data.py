009af48bc1f08db529e1f2080435fe034115d809
bourhan dernayka <hanodern@gmail.com>
2022-03-27 01:39:28 +0100

    fix pep8, update .gitignore

--- download_data.py
+++ download_data.py
@@ -3,7 +3,8 @@ import requests
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# file source: https://archive.ics.uci.edu/ml/machine-learning-databases/00526/data.zip
+# file source:
+# https://archive.ics.uci.edu/ml/machine-learning-databases/00526/data.zip
 url = "https://www.dropbox.com/s/gpz7730z5ta1g1p/BitcoinHeistData.csv?dl=1"
 
 public_path = "./data/public/"
@@ -13,12 +14,12 @@ file_path = os.path.join(private_path, file_name)
 
 try:
     os.stat(private_path)
-except OSError as e:
+except OSError as _:
     os.mkdir(private_path)
 
 try:
     os.stat(public_path)
-except OSError as e:
+except OSError as _:
     os.mkdir(public_path)
 
 print("Downloading the data file. This could take a couple of minutes...")
@@ -38,7 +39,8 @@ X_public, X_hidden, Y_public, Y_hidden = train_test_split(
 )
 
 X_train, X_test, Y_train, Y_test = train_test_split(
-    X_public, Y_public, random_state=42, test_size=0.3, stratify=Y_public, shuffle=True
+    X_public, Y_public, random_state=42, test_size=0.3,
+    stratify=Y_public, shuffle=True
 )
 
 X_hidden['label'] = Y_hidden
@@ -49,9 +51,12 @@ private_set = X_hidden
 public_train = X_train
 public_test = X_test
 
-private_set.to_csv(os.path.join(private_path, "private.csv.gzip"), index=False, compression="gzip")
-public_train.to_csv(os.path.join(public_path, "train.csv.gzip"), index=False, compression="gzip")
-public_test.to_csv(os.path.join(public_path, "test.csv.gzip"), index=False, compression="gzip")
+private_set.to_csv(os.path.join(private_path, "private.csv.gzip"),
+                   index=False, compression="gzip")
+public_train.to_csv(os.path.join(public_path, "train.csv.gzip"),
+                    index=False, compression="gzip")
+public_test.to_csv(os.path.join(public_path, "test.csv.gzip"),
+                   index=False, compression="gzip")
 
 os.remove(file_path)
 print("Data extracted...")
